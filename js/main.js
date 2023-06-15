bdf3f0da5e949aac93c9dc89d186f61ab20d2607
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-06-15 21:29:36 +0100

    new

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-/*  ---------------------------------------------------
-Template Name: Ashion
-Description: Ashion ecommerce template
-Author: Colorib
-Author URI: https://colorlib.com/
-Version: 1.0
-Created: Colorib
----------------------------------------------------------  */
+
 
 'use strict';
 
@@ -164,7 +157,7 @@ Created: Colorib
 
     // Uncomment below and use your date //
 
-    /* var timerdate = "2020/12/30" */
+    var timerdate = "2023/12/31" 
 
 	$("#countdown-time").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='countdown__item'><span>%D</span> <p>Day</p> </div>" + "<div class='countdown__item'><span>%H</span> <p>Hour</p> </div>" + "<div class='countdown__item'><span>%M</span> <p>Min</p> </div>" + "<div class='countdown__item'><span>%S</span> <p>Sec</p> </div>"));
